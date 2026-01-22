30f91f19aa06ad915f14aad9de796d8f725ab205
Abdul Rafay <abdulrafaycodeder@gmail.com>
2026-01-22 02:56:02 -0800

    javascript

--- script.js
+++ script.js
@@ -1,7 +1,19 @@
-var imageShow = 
+var imageShow = false
 
 function shopInage() {
     var showImage = "<img src='./images/hero-section-2-image.png' alt=''>"
+    var buttonText = "Click to hide image"
+    var hide = "Click to hide image"
+    
 
+    if (imageShow === false) {
+        document.getElementById("div").innerHTML = showImage
+        document.getElementById("showImage").innerText = buttonText
+        imageShow = true
+    } else {
+        document.getElementById("div").innerHTML = ""
+        document.getElementById("showImage").innerText = hide
+        imageShow = false
+    }
 
 }
